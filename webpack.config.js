a9874f488dac7202699a6485fdc7ca966a280bed
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-14 19:47:16 +0200

    Add source map+ web server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { watchFile } = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
@@ -9,6 +10,12 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+
+  devtool: "eval-source-map",
+
+  devServer: {
+    watchFiles: ["./src/template.html"],
   },
 
   plugins: [
