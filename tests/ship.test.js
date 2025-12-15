4c910fd7a9958768e5d125d79e8776d014399664
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-15 23:13:44 +0200

    Install jest dependencies
    Install jest dependencies 2

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -2,9 +2,9 @@
 // Test Main Game Modules
 // ===========================================================
 
-import { Ship } from "../src/modules/ship";
+import Ship from "../src/modules/ship";
 
-test('Ship starts with zero hits', () => {
+test("Ship starts with zero hits", () => {
   const ship = new Ship(3);
   expect(ship.hits).toBe(0);
 });
