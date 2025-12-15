430b16ff0078ce82aed0faef9c089dd25eeb648d
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-15 22:57:37 +0200

    Write test for Ship

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -2,7 +2,7 @@
 // Test Main Game Modules
 // ===========================================================
 
-import { Ship } from "../../src/modules/ship";
+import { Ship } from "../src/modules/ship";
 
 test('Ship starts with zero hits', () => {
   const ship = new Ship(3);
