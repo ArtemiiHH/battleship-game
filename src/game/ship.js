2fcdefb8ed4adc70aff4616f9c491d4c859be6b3
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-15 21:33:50 +0200

    Create hit and isSunk funcs + add comments

--- src/game/ship.js
+++ src/game/ship.js
@@ -1,7 +1,18 @@
+// ==============================================================
+// Ship Class
+// ==============================================================
+
+// Represents single ship
 class Ship {
   constructor(length, hit = 0, sunk = false) {
     this.length = length;
     this.hit = hit;
     this.sunk = sunk;
   }
+
+  // Increase number of hits in my ship
+  hit() {}
+
+  // Calculate whether ship is sunk, based on length and number of hits
+  isSunk() {}
 }
