e76aff87a227cc8f87b74b14a46e9da27eed4a20
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-15 22:52:32 +0200

    Remove modules directory in tests

---
+++ tests/ship.test.js
@@ -0,0 +1,10 @@
+// ===========================================================
+// Test Main Game Modules
+// ===========================================================
+
+import { Ship } from "../../src/modules/ship";
+
+test('Ship starts with zero hits', () => {
+  const ship = new Ship(3);
+  expect(ship.hits).toBe(0);
+});
