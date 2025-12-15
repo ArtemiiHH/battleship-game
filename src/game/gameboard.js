e48ab73231e86193a7e077a3fc2fe6ce2e8fe27d
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-15 22:01:31 +0200

    Create Gameboard class + add constructor, recieveAttack func

---
+++ src/game/gameboard.js
@@ -0,0 +1,10 @@
+// ===========================================================
+// Game Board Class
+// ===========================================================
+
+// Represents player's game board
+class Gameboard {
+  constructor() {}
+
+  receiveAttack() {}
+}
