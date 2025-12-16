05c6d0cba6b531ef721cd61092c29c06b4ad9ebd
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-16 16:04:55 +0200

    Add values in contructor

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -4,16 +4,19 @@
 
 // Represents player's game board
 export class Gameboard {
-  constructor() {}
+  constructor(size = 10) {
+    this.size = size;
+    this.board = this.createBoard();
+  }
 
   // Create 10 x 10 board
-  createBoard(size = 10) {
+  createBoard() {
     const board = [];
 
     // Create rows and columns
-    for (let row = 0; row < size; row++) {
+    for (let row = 0; row < this.size; row++) {
       board[row] = [];
-      for (let col = 0; col < size; col++) {
+      for (let col = 0; col < this.size; col++) {
         board[row][col] = null;
       }
     }
