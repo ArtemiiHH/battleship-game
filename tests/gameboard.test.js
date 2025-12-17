713c04a9d5be6c1f70fd07dd6efbe70851972694
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-17 14:09:55 +0200

    Write wrong test for placeShip

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -5,12 +5,22 @@
 import { Gameboard } from "../src/modules/gameboard";
 
 // Wrong test for now
-test('Return gameboard', () => {
+test("Return gameboard", () => {
   const gameboard = new Gameboard();
-  expect(gameboard.createBoard(size = 10)).toBe();
+  expect(gameboard.createBoard((size = 10))).toBe();
 });
 
 // Attacking an empty cell results in a miss
-test('Attacking an empty cell results in a miss', () => {
+test("Attacking an empty cell results in a miss", () => {
   expect(Gameboard.receiveAttack(0, 0)).toBe(false);
+});
+
+// Place ship function
+test("Check plaeShip function", () => {
+  expect(Gameboard.placeShip(ship = 3, 0, 0, 'vertical')).toBe([
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+  ]);
 });
