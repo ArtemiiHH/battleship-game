dc503abc008803aa3ef6f7ebe5ffc69a5624f5d1
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-21 17:49:30 +0200

    Remove missedAttack test too

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -8,9 +8,6 @@ import { Ship as ship } from "../src/modules/ship";
 // Test placeShip() function
 test("Check placeShip function", () => {});
 
-// Test missedAttack() function
-test("Do missed attacks get written down", () => {});
-
 // Test receiveAttack() function
 test("Attacking an empty cell results in a miss", () => {});
 test("Empty cell attack returns miss", () => {});
