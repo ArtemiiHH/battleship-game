b1e62620bdbf5b65404f490cfb7b04a628abea61
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-21 18:36:52 +0200

    Write correct test for placeShip func

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -2,11 +2,22 @@
 // Test Game Board Modules
 // ===========================================================
 
-import { Gameboard as gameboard } from "../src/modules/gameboard";
-import { Ship as ship } from "../src/modules/ship";
+import { Gameboard } from "../src/modules/gameboard";
+import { Ship } from "../src/modules/ship";
 
 // Test placeShip() function
-test("Place ship on valid coordinates", () => {});
+test("Ship placement", () => {
+  const gameboard = new Gameboard();
+  const ship = new Ship(3);
+
+  // Ship placement
+  expect(gameboard.placeShip(ship, 0, 0, "vertical")).toBe(true);
+  // Board contains the ship where expected
+  expect(gameboard.board[0][0]).toBe(ship);
+  expect(gameboard.board[1][0]).toBe(ship);
+  // Check the ship was passed in array
+  expect(gameboard.ships).toContain(ship);
+});
 
 // Test receiveAttack() function
 
