95f4be548dcb369c720e0feab3d2f9357dafb039
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-21 17:58:11 +0200

    Delete all old tests

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -6,14 +6,8 @@ import { Gameboard as gameboard } from "../src/modules/gameboard";
 import { Ship as ship } from "../src/modules/ship";
 
 // Test placeShip() function
-test("Check placeShip function", () => {});
+test("Place ship on valid coordinates", () => {});
 
 // Test receiveAttack() function
-test("Attacking an empty cell results in a miss", () => {});
-test("Empty cell attack returns miss", () => {});
-test("Record a hit", () => {});
-test("Check if missed", () => {});
-test("Check if all ships are sunk", () => {});
 
 // Test allShipsSunk() function
-test();
