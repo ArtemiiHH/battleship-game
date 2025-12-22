be1774dfdfa4c08f25b37e11141e3b88d5a548d3
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-22 19:40:59 +0200

    Write tests for human attacks

--- tests/player.test.js
+++ tests/player.test.js
@@ -8,12 +8,34 @@ import { Ship } from "../src/modules/ship";
 
 // Test attack() function
 describe("Human attack", () => {
-  const gameboard = new Gameboard();
-  const ship = new Ship(3);
-  const player = new Player('human player');
+  test("returns miss", () => {
+    const gameboard = new Gameboard();
+    const player = new Player("human player");
 
-  test('Human attack given coordinates and returns result', () => {
+    expect(player.attack(gameboard, 1, 0)).toBe("miss");
+  });
 
+  test("returns hit and increase hit count", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(3);
+    const player = new Player("human player");
+
+    gameboard.placeShip(ship, 0, 0, "vertical");
+
+    expect(player.attack(gameboard, 0, 0)).toBe("hit");
+    expect(ship.hits).toBe(1);
+  });
+
+  test("returns already-attacked and does not increase hit count", () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(3);
+    const player = new Player("human player");
+
+    gameboard.placeShip(ship, 0, 0, "vertical");
+
+    expect(player.attack(gameboard, 0, 0)).toBe("hit");
+    expect(player.attack(gameboard, 0, 0)).toBe("already-attacked");
+    expect(ship.hits).toBe(1);
   });
 });
 
