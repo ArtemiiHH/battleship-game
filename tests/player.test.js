c70284ac6665a1ccaece2dcdc7fdb6f7481b3326
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-22 18:21:33 +0200

    Split attack test in 2 groups using describe

--- tests/player.test.js
+++ tests/player.test.js
@@ -3,4 +3,10 @@
 // ===========================================================
 
 import { Gameboard } from "../src/modules/gameboard";
-import { Player } from "../src/modules/player"
+import { Player } from "../src/modules/player";
+import { Ship } from "../src/modules/ship";
+
+// Test attack() function
+describe("Human attack", () => {});
+
+describe("Computer attack", () => {});
