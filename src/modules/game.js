42a30c2d6137ce0f4cd22ffe902cda595f9104fb
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-25 18:36:17 +0200

    Assign turn + game state in constructor too

--- src/modules/game.js
+++ src/modules/game.js
@@ -8,9 +8,11 @@ import { Player } from "./player";
 
 // Full running match of Battleship
 export class Game {
-  constructor() {
-    this.playerOne = new Player('human');
-    this.playerTwo = new Player('computer', true);
+  constructor(isGameRunning = false) {
+    this.playerOne = new Player("human");
+    this.playerTwo = new Player("computer", true);
+    this.currentTurn = this.playerOne;
+    this.isGameRunning = isGameRunning;
   }
 
   start() {}
