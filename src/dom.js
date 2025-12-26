030c00ebdd36761339350d23aa4fb2cbf65fbefa
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-26 21:22:18 +0200

    Clean up code + place everything in their funcs

--- src/dom.js
+++ src/dom.js
@@ -4,24 +4,27 @@
 
 import { Game } from "./modules/game";
 
+// Start game
 const game = new Game();
 game.start();
 
 // App container
 const container = document.querySelector("#container");
 
+// Create board wrapper
 function createBoardWrapper(type) {
   const wrapper = document.createElement("div");
 
   if (type === "human") {
     wrapper.classList.add("human-wrapper");
-    return wrapper;
   } else {
     wrapper.classList.add("computer-wrapper");
-    return wrapper;
   }
+
+  return wrapper;
 }
 
+// Render board into wrapper
 function renderBoard(boardData, wrapper) {
   // Create board
   for (let i = 0; i < boardData.length; i++) {
@@ -38,9 +41,21 @@ function renderBoard(boardData, wrapper) {
   }
 }
 
+// Render entire game UI
 function renderGame(game) {
-  game.start();
-  createBoardWrapper();
-  renderBoard();
-  container.append(wrapper);
+  // Clear container
+  container.innerHTML = '';
+
+  // Create wrappers
+  const humanWrapper = createBoardWrapper('human');
+  const computerWrapper = createBoardWrapper('computer');
+
+  // Render boards
+  renderBoard(game.playerOne.board.board, humanWrapper);
+  renderBoard(game.playerTwo.board.board, computerWrapper);
+
+  container.append(humanWrapper, computerWrapper);
 }
+
+// Initial game render
+renderGame(game);
