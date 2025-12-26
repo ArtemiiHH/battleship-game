db953f106fb5d14519ef27f9297a8f9e87490a0e
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-26 18:19:38 +0200

    Create 3 funcs to clean up code

--- src/dom.js
+++ src/dom.js
@@ -4,6 +4,12 @@
 
 import { Game } from "./modules/game";
 
+function createBoardWrapper(type) {}
+
+function renderBoard(boardData, wrapepr) {}
+
+function renderGame(game) {}
+
 // Start game
 const game = new Game();
 game.start();
@@ -11,20 +17,20 @@ game.start();
 // App container
 const container = document.querySelector("#container");
 
+// Get player board data
+const humanBoard = game.playerOne.board.board;
+const computerBoard = game.playerTwo.board.board;
+
 // Board cell wrappers
 const humanBoardWrapper = document.createElement("div");
 humanBoardWrapper.classList.add("human-wrapper");
 const computerBoardWrapper = document.createElement("div");
 computerBoardWrapper.classList.add("computer-wrapper");
 
-// Get player board data
-const humanBoard = game.playerOne.board.board;
-const computerBoard = game.playerTwo.board.board;
-
 // Create human board (visual)
 for (let i = 0; i < humanBoard.length; i++) {
   const row = document.createElement("div");
-  row.classList.add('row');
+  row.classList.add("row");
 
   for (let j = 0; j < humanBoard[i].length; j++) {
     const cell = document.createElement("div");
@@ -39,7 +45,7 @@ container.appendChild(humanBoardWrapper);
 // Create computer board (visual)
 for (let i = 0; i < computerBoard.length; i++) {
   const row = document.createElement("div");
-  row.classList.add('row');
+  row.classList.add("row");
 
   for (let j = 0; j < computerBoard[i].length; j++) {
     const cell = document.createElement("div");
