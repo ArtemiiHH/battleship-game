f204e35ad089b0f370a0fcf8d9e1ab7bc1cd4aa6
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-12-30 16:22:44 +0200

    Write working tests for playerAttack

--- tests/game.test.js
+++ tests/game.test.js
@@ -3,6 +3,8 @@
 // ===========================================================
 
 import { Game } from "../src/modules/game";
+import { Ship } from "../src/modules/ship";
+import { Gameboard } from "../src/modules/gameboard";
 
 // Test start() function
 describe("Start function", () => {
@@ -11,6 +13,7 @@ describe("Start function", () => {
 
     game.start();
 
+    expect(game.isGameRunning).toBe(true);
     expect(game.playerOne.board).toBeDefined();
     expect(game.playerTwo.board).toBeDefined();
     expect(game.playerOne.board.ships.length).toBeGreaterThan(0);
@@ -23,5 +26,29 @@ describe("Start function", () => {
     game.start();
 
     expect(game.currentTurn).toBe(game.playerOne);
+  });
+});
+
+// Test playerAttack() function
+describe("Player attack", () => {
+  test("player attack hits a ship on the computer board", () => {
+    const game = new Game();
+    game.start();
+
+    const ship = new Ship("Test ship", 3);
+    game.playerTwo.board.placeShip(ship, 0, 0, "vertical");
+
+    game.playerAttack(0, 0);
+
+    expect(game.playerTwo.board.board[0][0]).toBe("hit");
+  });
+
+  test("player attack misses a ship on the computer board", () => {
+    const game = new Game();
+    game.start();
+
+    game.playerAttack(5, 5);
+
+    expect(game.playerTwo.board.board[5][5]).toBe("miss");
   });
 });
